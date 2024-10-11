112e016f251306e4d424cd58e308abad0917db1f
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-10-11 17:49:54 -0600

    feat: Agregar estructuras de flujo en control-flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@app/shared/title/title.component';
+
+type Grade = 'A' | 'B' | 'C' | 'D';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './control-flow.component.html',
   styles: `
     :host {
@@ -15,4 +16,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ControlFlowComponent { }
+export default class ControlFlowComponent {
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public frameworks = signal<string[]>(['Angular', 'React', 'Vue']);
+
+  public toggleContent() {
+    this.showContent.update((prev) => !prev);
+  }
+
+  public randomGrade() {
+    const grades: Grade[] = ['A', 'B', 'C', 'D'];
+    const randomIndex = Math.floor(Math.random() * grades.length);
+    this.grade.set(grades[randomIndex]);
+  }
+}
