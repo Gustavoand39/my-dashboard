0a894d3213b3adc7ce36099b55ddc238f32edd7b
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-10-11 17:03:55 -0600

    feat: Actualizar componentes de la barra lateral y agregar rutas relativas

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '@app/app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
