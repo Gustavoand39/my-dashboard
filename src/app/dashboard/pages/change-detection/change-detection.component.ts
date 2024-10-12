62c53f91b026aee5f701ac13155ac1d6c5ef3015
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-10-11 18:20:14 -0600

    feat: Agregar la detección de cambios en change-detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,16 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '@app/shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: `./change-detection.component.html`,
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="currentFramework()" />
+    <pre> {{ frameworkSignal() | json }} </pre>
+    <pre> {{ frameworkProperty | json }} </pre>
+  `,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change Detection = ${this.frameworkSignal().name}`
+  );
+
+  public frameworkSignal = signal({
+    name: 'Angular',
+    releaseDate: '2016',
+  });
+
+  public frameworkProperty = {
+    name: 'React',
+    releaseDate: '2013',
+  };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkSignal.update((prev) => ({
+        ...prev,
+        name: 'Vue',
+      }));
+    }, 3000);
+  }
+}
